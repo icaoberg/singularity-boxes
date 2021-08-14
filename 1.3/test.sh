c32eed4a8b08ee1bef4aee021cc7d7257e05590d
icaoberg <icaoberg@andrew.cmu.edu>
2021-08-14 09:20:54 -0400

    Update test.sh

--- 1.3/test.sh
+++ 1.3/test.sh
@@ -2,4 +2,4 @@
 
 tree .
 
-echo -e '\"Spartans! Ready your breakfast and eat hearty, for tonight, we dine in hell!\"' | singularity exec singularity-boxes-1.3.sif boxes -d peek -a c -s 40x11
+echo -e '"Spartans! Ready your breakfast and eat hearty, for tonight, we dine in hell!"' | singularity exec singularity-boxes-1.3.sif boxes -d peek -a c -s 40x11
